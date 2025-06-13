716ebb4911e6f6ccf3028b906789d272a39ca5fd
os <os@ubuntuweb>
2025-06-13 19:09:56 +0000

    Restored repeatable background with proper opacity and size

--- tosia/settings.py
+++ tosia/settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'pl'
 
 LANGUAGES = [
     ('en', 'English'),
